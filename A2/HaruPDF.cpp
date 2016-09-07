62fb69b913e258c8566eac8219c669a942fffaff
Tyler K Dailey <tkdailey11@gmail.com>
2016-09-07 17:48:05 -0600

    Added Copyright Information to HaruPDF.cpp, as well as reorganized the includes for HaruPDF.cpp to conform to the standard convention of matching header first.

--- A2/HaruPDF.cpp
+++ A2/HaruPDF.cpp
@@ -1,15 +1,28 @@
 /*
- * Fascade Class written to provide more convenient access to the HaruPDF C library.
- * Written By: Tyler Dailey, 9/6/16
+ * << Alternative PDF Library 1.0.0 >> -- text_demo2.c
+ *
+ * Copyright (c) 1999-2006 Takeshi Kanno <takeshi_kanno@est.hi-ho.ne.jp>
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ * It is provided "as is" without express or implied warranty.
+ *
+ * Modified by Tyler Dailey, University of Utah, 2016.
+ *
+ * This is a Fascade Class written to provide more convenient access to the HaruPDF library mentioned above.
+ * Written By: Tyler Dailey, 9/7/16
  * Assignment 2, CS3505
  */
 
+#include "HaruPDF.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include "hpdf.h"
-#include "HaruPDF.h"
 
 using namespace std;
 
